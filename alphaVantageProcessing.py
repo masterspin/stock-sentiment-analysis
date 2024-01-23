2bc2dae9d1aac762d07e4023fc0c3727c5bcff49
Ritij Jutur <rjutur@umich.edu>
2024-01-22 23:34:48 -0500

    finished processing daily returns for each stock in 2023

--- alphaVantageProcessing.py
+++ alphaVantageProcessing.py
@@ -8,10 +8,8 @@ finnhubDF = pd.read_csv('companyNews.csv')
 
 finnhubDF['language'] = finnhubDF['headline'].apply(lambda x: detect(str(x)) if pd.notna(x) else None)
 
-# Drop rows where language is not English
 finnhubDF = finnhubDF[finnhubDF['language'] == 'en']
 
-# Drop the 'language' column if you don't need it anymore
 finnhubDF = finnhubDF.drop(columns=['language'])
 
 finnhubDF.to_csv("Filtered_companyNews.csv", index=False)
--- yfinanceProcessing.py
+++ yfinanceProcessing.py
@@ -1,18 +1,31 @@
 import yfinance as yf
+import pandas as pd
 
-# Define the stock symbol and time period
-stock_symbol = "AAPL"
+tickersString = "MMM,AOS,ABT,ABBV,ACN,ADBE,AMD,AES,AFL,A,APD,ABNB,AKAM,ALB,ARE,ALGN,ALLE,LNT,ALL,GOOGL,GOOG,MO,AMZN,AMCR,AEE,AAL,AEP,AXP,AIG,AMT,AWK,AMP,AME,AMGN,APH,ADI,ANSS,AON,APA,AAPL,AMAT,APTV,ACGL,ADM,ANET,AJG,AIZ,T,ATO,ADSK,ADP,AZO,AVB,AVY,AXON,BKR,BALL,BAC,BK,BBWI,BAX,BDX,BRK-B,BBY,BIO,TECH,BIIB,BLK,BX,BA,BKNG,BWA,BXP,BSX,BMY,AVGO,BR,BRO,BF-B,BLDR,BG,CDNS,CZR,CPT,CPB,COF,CAH,KMX,CCL,CARR,CTLT,CAT,CBOE,CBRE,CDW,CE,COR,CNC,CNP,CDAY,CF,CHRW,CRL,SCHW,CHTR,CVX,CMG,CB,CHD,CI,CINF,CTAS,CSCO,C,CFG,CLX,CME,CMS,KO,CTSH,CL,CMCSA,CMA,CAG,COP,ED,STZ,CEG,COO,CPRT,GLW,CTVA,CSGP,COST,CTRA,CCI,CSX,CMI,CVS,DHR,DRI,DVA,DE,DAL,XRAY,DVN,DXCM,FANG,DLR,DFS,DG,DLTR,D,DPZ,DOV,DOW,DHI,DTE,DUK,DD,EMN,ETN,EBAY,ECL,EIX,EW,EA,ELV,L"
+
+tickers = tickersString.split(",")
+
 start_date = "2022-12-30"
 end_date = "2023-12-31"
 
-# Retrieve historical stock prices
-stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
+all_returns = []
 
-# Calculate daily returns using the previous day's close
-stock_data['Daily_Return'] = (stock_data['Close'] - stock_data['Close'].shift(1)) / stock_data['Close'].shift(1) * 100
+for stock_symbol in tickers:
+    stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
+    stock_data['Daily_Return'] = (stock_data['Close'] - stock_data['Close'].shift(1)) / stock_data['Close'].shift(1) * 100
+    stock_data = stock_data.dropna()
+    
+    # Extract the date and daily return columns, and rename the daily return column with the ticker symbol
+    stock_data = stock_data[['Daily_Return']].rename(columns={'Daily_Return': stock_symbol})
+    
+    all_returns.append(stock_data)
 
-# Drop the first row since it will have NaN values
-stock_data = stock_data.dropna()
+# Concatenate all DataFrames in the list
+all_returns = pd.concat(all_returns, axis=1)
 
-# Display the resulting DataFrame
-print(stock_data[['Close', 'Daily_Return']])
+print(all_returns)
+
+#accessing the dataframe example
+target_stock = 'EBAY'
+target_date = '2023-12-22'
+print(all_returns.loc[target_date, target_stock])
